3afcb3cdcbbb942b2bb36d8e458e06d1e0c73012
Ahmed Essam <40560225+iRashwan@users.noreply.github.com>
2025-04-05 08:30:02 +0200

    fixed api issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://wardaweb-001-site1.qtempurl.com/api/',
+    baseUrl: 'https://wardaweb-001-site1.qtempurl.com/',
     // baseUrl: "https://abdoryad-001-site1.ktempurl.com/api/",
      baseImageUrl: 'https://wardaweb-001-site1.qtempurl.com',
     // baseImageUrl: "https://abdoryad-001-site1.ktempurl.com"
