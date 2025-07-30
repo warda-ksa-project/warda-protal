525b0faf057bf7c83e592bc2bfa60daad62aeb52
Ahmed Essam <40560225+iRashwan@users.noreply.github.com>
2025-07-31 01:25:55 +0300

    fix production version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://backend.wrdah.com/api/',
+    baseUrl: 'https://backend.wrdah.com/',
     // baseUrl: "https://abdoryad-001-site1.ktempurl.com/api/",
      baseImageUrl: 'https://backend.wrdah.com',
     // baseImageUrl: "https://abdoryad-001-site1.ktempurl.com"
